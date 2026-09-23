5edc03f14e8d578f718fd1550d283c40276ef532
Qboo-star <2842850273@qq.com>
2026-09-23 19:40:02 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -42,6 +42,5 @@ public interface DishMapper {
     @AutoFill(value = OperationType.UPDATE)
     void updateById(Dish dish);
 
-    @Select("select * from dish where category_id = #{categoryId} and status = 1")
-    List<Dish> list(Long categoryId);
+    List<Dish> list(Dish dish);
 }
