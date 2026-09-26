497851793584148b4018a0d2a4c076cb237c915f
Qboo-star <2842850273@qq.com>
2026-09-26 16:57:47 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,7 @@ import com.sky.vo.DishVO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +23,9 @@ import java.util.List;
 @RequestMapping("/admin/dish")
 @Slf4j
 public class DishController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Autowired
     private DishService dishService;
     @PostMapping
@@ -66,8 +70,17 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation(value = "根据分类查询菜品", notes = "根据分类id查询起售中的菜品")
     public Result<List<Dish>> list(Long categoryId){
+        //构造redis
+        String key = "dish:list:" + categoryId;
+        //查询redis中是否有缓存
+        List<Dish> cache = (List<Dish>) redisTemplate.opsForValue().get(key);
+        if (cache != null && cache.size() > 0  ) {
+            return Result.success(cache);
+        }
+
         log.info("根据分类id查询菜品：{}", categoryId);
         List<Dish> list = dishService.list(categoryId);
+        redisTemplate.opsForValue().set(key, list);
         return Result.success(list);
     }
 }
